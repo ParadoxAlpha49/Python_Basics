bd72992c167f4a37ffdb5ccf7792edc5a90e5cf9
Sazzad Sarker <66535042+ParadoxAlpha49@users.noreply.github.com>
2020-10-09 18:03:26 +0600

    while loop ( sum problem )

---
+++ whileloopsinpython.py
@@ -0,0 +1,14 @@
+#while loop
+#while [condition]:
+#	[statements]
+#	else:
+# 1-20 numbers 
+count = 1 
+sum = 0 
+
+while count <=20:
+	sum = sum + count
+	count = count + 1
+print(sum)
+
+#for value in [iterable datatype]
